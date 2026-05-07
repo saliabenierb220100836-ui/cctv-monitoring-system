76ccb5570ac2a8d00932ab9fc0cbf0a3762826e8
saliabenierb220100836-ui <saliabenierb220100836@gmail.com>
2026-05-07 11:35:57 +0800

    railyway changes

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ def create_app():
     db.init_app(app)
 
     login_manager.init_app(app)
-
+    
     from app.routes.main_routes import main
     app.register_blueprint(main)
 
--- config.py
+++ config.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
