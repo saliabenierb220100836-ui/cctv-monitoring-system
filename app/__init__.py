b1f3ef4bbee01ae4a6db3d2d2039f737fc6b659d
saliabenierb220100836-ui <saliabenierb220100836@gmail.com>
2026-05-07 10:28:12 +0800

    Initial Commit
    network administration group7

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
+
+db = SQLAlchemy()
+
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    login_manager.init_app(app)
+
+    from app.routes.main_routes import main
+    app.register_blueprint(main)
+
+    from app.models.user import User
+    from app.models.log import AuditLog
+
+    return app
---
+++ app/models/log.py
@@ -0,0 +1,14 @@
+from app import db
+from datetime import datetime
+
+class AuditLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    action = db.Column(db.String(255))
+
+    ip_address = db.Column(db.String(50))
+
+    timestamp = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
