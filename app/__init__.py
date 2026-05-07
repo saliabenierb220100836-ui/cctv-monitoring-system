87c72026f7ea84154813ff3f73b2424129b72a96
saliabenierb220100836-ui <saliabenierb220100836@gmail.com>
2026-05-07 20:48:03 +0800

    time log

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -11,11 +13,23 @@ def load_user(user_id):
     from app.models.user import User
     return User.query.get(int(user_id))
 
+
+def format_ph_time(value, fmt='%Y-%m-%d %H:%M:%S'):
+    if value is None:
+        return ''
+    if not isinstance(value, datetime):
+        return value
+    if value.tzinfo is None:
+        value = value.replace(tzinfo=ZoneInfo('UTC'))
+    return value.astimezone(ZoneInfo('Asia/Manila')).strftime(fmt)
+
+
 def create_app():
     app = Flask(__name__)  # No spaces before 'app'
     app.config.from_object(Config)
 
     app.jinja_env.cache = {}
+    app.jinja_env.filters['ph_time'] = format_ph_time
 
     db.init_app(app)
     login_manager.init_app(app)
