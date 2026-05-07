f2aa08249310dafb81d09c9cb127e5dfb0b1ef1b
saliabenierb220100836-ui <saliabenierb220100836@gmail.com>
2026-05-07 22:16:30 +0800

    Update log.py

--- app/models/log.py
+++ app/models/log.py
@@ -6,14 +6,13 @@ def get_pht_now():
     """Returns current time in Philippine Time"""
     return datetime.now(pytz.timezone('Asia/Manila'))
 
-class AuditLog(db.Model):
+class AuditLog(db.Model): # Must be 'AuditLog' to match your main_routes.py import
     id = db.Column(db.Integer, primary_key=True)
-    action = db.Column(db.String(50), nullable=False) # 'Login' or 'Logout'
+    action = db.Column(db.String(50), nullable=False)
     ip_address = db.Column(db.String(50), nullable=False)
     timestamp = db.Column(db.DateTime, default=get_pht_now)
-    
-    # Links to the User model
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # This is the line the error is complaining about:
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
     user = db.relationship('User', backref=db.backref('activity_logs', lazy=True))
 
     def __repr__(self):
