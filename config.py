647fd8e4b7fdd37678aeb7f4f1e820ca0cea44ab
saliabenierb220100836-ui <saliabenierb220100836@gmail.com>
2026-05-14 11:19:59 +0800

    db

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://postgres:rysPxkSTIrXgEwDAsBJaCdieHjMUUwfm@postgres.railway.internal:5432/') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
     WTF_CSRF_ENABLED = False
