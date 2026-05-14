8b5133d3381a037532599a95ec2479f1b9f26863
Kristel <kristelmalabanan36@gmail.com>
2026-05-14 11:57:13 +0800

    fixes

--- config.py
+++ config.py
@@ -4,13 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://postgres:rysPxkSTIrXgEwDAsBJaCdieHjMUUwfm@turntable.proxy.rlwy.net:12704/railway_DB}}') or 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        "connect_args": {
-            "sslmode": "require"
+    # Only set SSL options for PostgreSQL
+    if SQLALCHEMY_DATABASE_URI.startswith('postgresql'):
+        SQLALCHEMY_ENGINE_OPTIONS = {
+            "connect_args": {
+                "sslmode": "require"
+            }
         }
-    }
