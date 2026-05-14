e1231aec1be27acd96eb3200c3f2f72ec918213e
saliabenierb220100836-ui <saliabenierb220100836@gmail.com>
2026-05-14 12:11:27 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -29,7 +30,7 @@ def ensure_database_schema():
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    app.secret_key = os.environ.get("SECRET_KEY")
     app.jinja_env.cache = {}
 
     db.init_app(app)
