bc0f496627c1b0139ee7b36a9fa93edf8a1c71fb
saliabenierb220100836-ui <saliabenierb220100836@gmail.com>
2026-05-14 11:44:04 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://postgres:rysPxkSTIrXgEwDAsBJaCdieHjMUUwfm@turntable.proxy.rlwy.net:12704/railway_DB}}') or 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'SQLALCHEMY_DATABASE_URI'
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    WTF_CSRF_ENABLED = False
-    TEMPLATES_AUTO_RELOAD = True
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "sslmode": "require"
+        }
+    }
